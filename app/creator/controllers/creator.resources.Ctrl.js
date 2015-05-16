8248dcc83cf509bbdf208e308742c46519c8ef59
Knirunmund <matthewsickler@gmail.com>
2015-05-16 17:24:18 -0400

    Logic of resource form complete, just need to finish adding all the inputs

--- app/creator/controllers/creator.resources.Ctrl.js
+++ app/creator/controllers/creator.resources.Ctrl.js
@@ -3,34 +3,43 @@
 
 var creatorResourcesController = angular.module('creatorResourcesController', []);
 
-creatorResourcesController.controller('creator.resources.Ctrl', ['$scope','$stateParams','$state',
-function($scope,$stateParams,$state) {
-  console.log("Resources Controller");
-  $state.current.data.test.stateChangeCount++;
+creatorResourcesController.controller('creator.resources.Ctrl', ['$scope','$stateParams','$state','$filter',
+function($scope,$stateParams,$state,$filter) {
+  $scope.resourceInfo = {};
 
-  $scope.resourceInfo = {
-    resourceName: ""
-  };
-
-  $scope.addResource = function(form) {
-    console.log(form);
+  $scope.submit = function(form) {
+    //If form is invalid, don't add/update the resource
     if(form.$invalid) {
-      alert("Form is invalid");
       return;
     }
 
-    $state.current.data.creator.resources.push({name: $scope.resourceInfo.resourceName});
-    $scope.resourceInfo.resourceName = "";
-  };
-
-  $scope.remove = function(resource) {
+    //Look forexisting resource with the given name for an update
+    var found = false;
     for(var x = 0; x < $state.current.data.creator.resources.length; x++) {
-      if($state.current.data.creator.resources[x].name === resource.name) {
-        $state.current.data.creator.resources.splice(x, 1);
-        $scope.$broadcast('distinctName', "creator.resources");
+      if($state.current.data.creator.resources[x].name === $scope.resourceInfo.name) {
+        $state.current.data.creator.resources[x] = angular.copy($scope.resourceInfo);
+        found = true;
         break;
       }
     }
+
+    //Add new resource if not updating an existing resource
+    if(!found) {
+      $state.current.data.creator.resources.push(angular.copy($scope.resourceInfo));
+      $state.current.data.creator.resources = $filter('orderBy')($state.current.data.creator.resources, 'name', false);
+    }
+
+    //Set inputs to empty and set form to un-submitted
+    $scope.resourceInfo = {};
+    form.$submitted = false;
+  };
+
+  $scope.remove = function($index) {
+    $state.current.data.creator.resources.splice($index, 1);
+  };
+
+  $scope.edit = function($index) {
+    $scope.resourceInfo = angular.copy($state.current.data.creator.resources[$index]);
   };
 
 
